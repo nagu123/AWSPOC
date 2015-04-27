5758658ec0726f9dccea68321a28ee6de09adf66
Paidipati Naga Raju <naga.rajux.paidipati@intel.com>
2015-04-27 19:18:52 +0530

    Made changes

--- SimpleJspServletDB/src/com/daniel/util/DbUtil.java
+++ SimpleJspServletDB/src/com/daniel/util/DbUtil.java
@@ -3,6 +3,7 @@ package com.daniel.util;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -16,24 +17,27 @@ public class DbUtil {
             return connection;
         else {
             try {
+            
             	Properties prop = new Properties();
-                InputStream inputStream = DbUtil.class.getClassLoader().getResourceAsStream("/db.properties");
-                prop.load(inputStream);
-                String driver = prop.getProperty("driver");
-                String url = prop.getProperty("url");
-                String user = prop.getProperty("user");
-                String password = prop.getProperty("password");
+           //     InputStream inputStream = DbUtil.class.getClassLoader().getResourceAsStream("/db.properties");
+            	//InputStream inputStream = DbUtil.class.getResourceAsStream("db.properties");            
+           //     prop.load(inputStream);               
+                String driver = "com.mysql.jdbc.Driver";
+                String url = "jdbc:mysql://localhost:3306/UserDB";
+                String user = "testuser1";
+                String password = "password";
+                System.out.println(driver +"--------" + url + "user = " +  user + " password = " + password);
                 Class.forName(driver);
                 connection = DriverManager.getConnection(url, user, password);
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             } catch (SQLException e) {
                 e.printStackTrace();
-            } catch (FileNotFoundException e) {
+            }/* catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
-            }
+            }*/
             return connection;
         }
 
